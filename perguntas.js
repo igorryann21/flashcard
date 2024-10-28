c212437c394753bf21cf4afdc66428c3fa4193bd
igorryann21 <00001245545462sp@al.educacao.sp.gov.br>
2024-10-28 14:37:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,163 @@
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico da água?',
+    'H₂O');
+
+criaCartao(
+    'Biologia',
+    'Qual é a unidade fundamental da vida?'
+    , 'Célula');
+
+criaCartao(
+    'Física',
+    'Qual é a velocidade da luz no vácuo?',
+    'Aproximadamente 3 x 10⁸ m/s');
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'Ásia');
+
+criaCartao(
+    'Geografia',
+    'Qual é o oceano mais profundo?',
+    'Oceano Pacífico');
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'Paris');
+
+
+criaCartao(
+    'História'
+    , 'Quem inventou a lâmpada incandescente?',
+    'Thomas Edison')
+
+criaCartao(
+    'História',
+    'Qual foi a primeira guerra mundial?',
+    'Primeira Guerra Mundial')
+
+criaCartao(
+    'História',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo da Vinci')
+
+
+criaCartao(
+    'Matemática',
+    'Quanto é 10 x 10?',
+    '100')
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 16?',
+    '4')
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de pi?',
+    'Aproximadamente 3,14')
+
+criaCartao(
+    'Português',
+    'Qual é o sinônimo de grande?',
+    'Grande');
+
+criaCartao(
+    'Português',
+    'Qual é o antônimo de claro?',
+    'Escuro');
+
+criaCartao(
+    'Português',
+    'Qual é o plural de lápis?',
+    'Lápis');
+
+
+criaCartao(
+    'Artes',
+    'Qual é o instrumento musical mais utilizado na música clássica?',
+    'Violino');
+
+
+criaCartao(
+    'Artes',
+    'Quem pintou a Capela Sistina?',
+    'Michelangelo');
+
+
+criaCartao(
+    'Artes',
+    'Qual é o estilo de dança caracterizado por movimentos rápidos e isolados?',
+    'Hip hop');
+
+
+criaCartao(
+    'Filosofia',
+    'Quem disse "Penso, logo existo"?',
+    'René Descartes');
+
+
+
+criaCartao(
+    'Música',
+    'Qual é a nota musical mais alta?',
+    'Dó');
+
+criaCartao(
+    'Tecnologia',
+    'Qual foi o primeiro navegador web amplamente utilizado?',
+    'Mosaic');
+
+
+criaCartao(
+    'Filosofia',
+    'Quem foi o fundador do budismo?',
+    'Siddharta Gautama')
+
+criaCartao(
+    'Economia',
+    'O que é deflação?',
+    'Redução generalizada e contínua dos preços')
+
+criaCartao(
+    'Tecnologia',
+    'Qual é a diferença entre software livre e software proprietário?',
+    'Software livre pode ser livremente usado, copiado e modificado; software proprietário tem seus direitos reservados')
+
+criaCartao(
+    'Psicologia',
+    'O que é a teoria da psicanálise?',
+    'Teoria desenvolvida por Sigmund Freud que busca compreender o funcionamento da mente humana',
+    ' especialmente o inconsciente')
+
+
+
+criaCartao(
+    'História',
+    'Qual foi a civilização que criou a escrita cuneiforme?',
+    'Sumérios')
+
+criaCartao(
+    'História',
+    'Quem liderou a Reforma Protestante?',
+    'Martin Lutero')
+
+
+criaCartao(
+    'História',
+    'Qual foi o evento que marcou o início da Idade Média?',
+    'Queda do Império Romano do Ocidente')
+
+
+criaCartao('História',
+    'Qual foi a Guerra Fria?',
+    'Conflito geopolítico e ideológico entre Estados Unidos e União Soviética após a Segunda Guerra Mundial')
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'Nilo')
+
